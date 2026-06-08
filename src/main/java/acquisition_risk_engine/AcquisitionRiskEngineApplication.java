8334ce88803100604364e9832d06dc4cfbb9d602
Alex Moozhayil <alexmoozhayil@mac.lan>
2026-06-08 11:44:50 -0500

    Add initial Spring Boot health endpoint

---
+++ src/main/java/acquisition_risk_engine/AcquisitionRiskEngineApplication.java
@@ -0,0 +1,13 @@
+package acquisition_risk_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquisitionRiskEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquisitionRiskEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/acquisition_risk_engine/AcquisitionRiskEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package acquisition_risk_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcquisitionRiskEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
